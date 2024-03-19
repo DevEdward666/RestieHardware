6ae5ecc27748ca08db22e2405584bc3513b0d05f
devEdward666 <edwardjosephfernandez@gmail.com>
2024-03-19 22:18:36 +0800

    update backend

--- RestieAPI/RestieAPI/obj/Debug/net8.0/RestieAPI.AssemblyInfo.cs
+++ RestieAPI/RestieAPI/obj/Debug/net8.0/RestieAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestieAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09705983c01ddc84f4182ad479194351f30c4da")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestieAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestieAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

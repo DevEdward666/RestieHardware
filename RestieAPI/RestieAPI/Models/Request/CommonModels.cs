0a69c3dc8757e7caf0adf6ad59070d6d17230bb3
devEdward666 <edwardjosephfernandez@gmail.com>
2024-03-22 00:44:13 +0800

    chore(): fix error

---
+++ RestieAPI/RestieAPI/Models/Request/CommonModels.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+
+namespace RestieAPI.Models.Request
+{
+    public class CommonModels
+    {
+        public class ResponseModelNonQuery{
+
+            public int reader { get; set; }
+            public Boolean success { get; set; }
+            public int statusCode { get; set; }
+        }
+        public class ResponseModelReader
+        {
+
+            public NpgsqlDataReader reader { get; set; }
+            public Boolean success { get; set; }
+            public int statusCode { get; set; }
+        }
+        
+    }
+}
--- RestieAPI/RestieAPI/obj/Debug/net8.0/RestieAPI.AssemblyInfo.cs
+++ RestieAPI/RestieAPI/obj/Debug/net8.0/RestieAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestieAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea49daa050a2feb10040511df890cdb883d16451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestieAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestieAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

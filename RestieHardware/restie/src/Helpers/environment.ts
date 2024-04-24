cb9bc3ee65f78b1069148311da75d4a30ff10ebc
devEdward666 <edwardjosephfernandez@gmail.com>
2024-04-24 22:17:55 +0800

    chore(): added checking of branch

--- RestieHardware/restie/src/Helpers/environment.ts
+++ RestieHardware/restie/src/Helpers/environment.ts
@@ -1,5 +1,17 @@
 import { devBaseUrl } from "./environment.dev";
 import { prodBaseUrl } from "./environment.prod";
-// environment.ts
-export const baseUrl =
-  process.env.NODE_ENV === "production" ? prodBaseUrl : devBaseUrl;
+
+let baseUrl;
+
+if (process.env.NODE_ENV === "production") {
+  baseUrl = prodBaseUrl;
+} else {
+  baseUrl = devBaseUrl;
+}
+
+// Add an additional check to conditionally select base URL based on GitHub branch
+if (process.env.REACT_APP_GITHUB_BRANCH === "refs/heads/release") {
+  baseUrl = prodBaseUrl;
+}
+
+export { baseUrl };

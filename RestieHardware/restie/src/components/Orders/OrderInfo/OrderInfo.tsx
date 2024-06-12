ac73067efa23b487546308d5dccbce929443b1eb
devEdward666 <edwardjosephfernandez@gmail.com>
2024-06-12 23:36:59 +0800

    no message

--- RestieHardware/restie/src/components/Orders/OrderInfo/OrderInfo.tsx
+++ RestieHardware/restie/src/components/Orders/OrderInfo/OrderInfo.tsx
@@ -624,7 +624,7 @@ const OrderInfoComponent = () => {
     const printData = encoder
       .initialize()
       // .image(logo, 296, 296, "atkinson") // Adjust width to 296 (multiple of 8)
-      .text(receiptText, 296)
+      .text(receiptText, 320)
       .encode();
     // await samplePrint();
     await printWithBluetooth(printData);
@@ -723,7 +723,7 @@ const OrderInfoComponent = () => {
       receiptText += `Order Created:${orderDate}\n`;
       receiptText += `Cashier:${order_list_info.order_info.createdby}\n`;
       receiptText += `Transaction ID:${order_list_info.order_info.transid}\n`;
-      receiptText += `------------------------------------------\n\n`;
+      receiptText += `--------------------------------\n\n`;
       // Initialize chunks array
       const chunks: string[] = [];
       let chunk = "";
@@ -739,7 +739,7 @@ const OrderInfoComponent = () => {
         // Add item details to receipt
         chunk += `${item.item} - ${item.price.toFixed(2)} PHP\n`;
         chunk += `Qty: ${item.qty}\n`;
-        chunk += `------------------------------------------\n\n`;
+        chunk += `--------------------------------\n\n`;
 
         // Check if adding the current item exceeds the limit
         if (
